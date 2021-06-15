0c9123439c5e0c0e5123a9983cbd153fd8de47fc
User <User@DESKTOP-LP2981H>
2021-06-15 14:49:25 -0500

    Página de proceso de pedido

---
+++ StoreDSWI.Web/ViewModels/OrdersViewModels.cs
@@ -0,0 +1,24 @@
+﻿using StoreDSWI.Entities;
+using StoreDSWI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreDSWI.Web.ViewModels
+{
+    public class OrdersViewModels
+    {
+        public List<Order> Orders { get; set; }
+        public string UserID { get; set; }
+        public Pager Pager { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class OrderDetailsViewModels
+    {
+        public Order Order { get; set; }
+        public ApplicationUser OrderBy { get; set; }
+        public List<string> AvailableStatuses { get; set; }
+    }
+}
